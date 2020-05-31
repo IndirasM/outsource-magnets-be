80999d32d93997f0c6e98c8e85458d3d7eef32bf
IndirasM <I.Maziukas@gmail.com>
2020-05-31 20:14:19 +0300

    add description to subject

--- src/main/java/com/psk/demo/Controller/Model/SubjectModel.java
+++ src/main/java/com/psk/demo/Controller/Model/SubjectModel.java
@@ -6,10 +6,12 @@ public class SubjectModel {
     public String name;
     public Long id;
     public Long parentId;
+    public String description;
 
     public SubjectModel(Subject subject) {
         this.name = subject.getName();
         this.id = subject.getId();
+        this.description = subject.getDescription();
         if(subject.getParentSubject() != null) {
             this.parentId = subject.getParentSubject().getId();
         }
