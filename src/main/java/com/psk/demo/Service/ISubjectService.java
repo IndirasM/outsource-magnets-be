3667f52f4762e38ab12cad03b34eaac0a056f5e4
IndirasM <I.Maziukas@gmail.com>
2020-06-02 01:41:20 +0300

    feat: create subject endpoint

--- src/main/java/com/psk/demo/Service/ISubjectService.java
+++ src/main/java/com/psk/demo/Service/ISubjectService.java
@@ -1,5 +1,6 @@
 package com.psk.demo.Service;
 
+import com.psk.demo.Controller.Model.NewSubjectModel;
 import com.psk.demo.Entity.Subject;
 
 import java.util.List;
@@ -8,4 +9,5 @@ import java.util.Optional;
 public interface ISubjectService {
 	List<Subject> findAll();
 	Optional<Subject> findById(Long id);
+	Long Insert(NewSubjectModel subject) throws Exception;
 }
